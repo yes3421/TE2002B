1a65de93c05345ce0b17291ec57ed2612185f22e
yes3421 <77082896+yes3421@users.noreply.github.com>
2023-02-28 07:54:45 -0600

    Add files via upload

---
+++ bcd_to_seven_segment_decoder.v
@@ -0,0 +1,33 @@
+/*
+ * BCD to Seven Segment Decoder
+ * 
+ * Paul Enrique Alonso Ramírez A01634608
+ * Diseño con lógica programable
+ * Grupo 101
+ * Tecnológico de Monterrey, Campus Guadalajara
+ * Viernes 24 de febrero de 2023
+ */
+
+module bcd_to_seven_segment_decoder (
+        input wire [3:0] bcd,
+        output reg [6:0] seven_segment
+);
+
+always @ (bcd)
+begin
+        case (bcd)
+        4'd0: seven_segment = 7'b1000000;
+        4'd1: seven_segment = 7'b1111001;
+        4'd2: seven_segment = 7'b0100100;
+        4'd3: seven_segment = 7'b0110000;
+        4'd4: seven_segment = 7'b0011001;
+        4'd5: seven_segment = 7'b0010010;
+        4'd6: seven_segment = 7'b0000010;
+        4'd7: seven_segment = 7'b1111000;
+        4'd8: seven_segment = 7'b0000000;
+        4'd9: seven_segment = 7'b0010000;
+        default: seven_segment = 7'b1111111;
+        endcase
+end
+
+endmodule
---
+++ bcd_to_seven_segment_decoder_testbench.v
@@ -0,0 +1,39 @@
+/*
+ * BCD to Seven Segment Decoder
+ * 
+ * Paul Enrique Alonso Ramírez A01634608
+ * Diseño con lógica programable
+ * Grupo 101
+ * Tecnológico de Monterrey, Campus Guadalajara
+ * Viernes 24 de febrero de 2023
+ */
+
+`timescale 1 ns / 1 ps
+
+module bcd_to_seven_segment_decoder_testbench ();
+
+reg [3:0] bcd;
+wire [6:0] seven_segment;
+
+bcd_to_seven_segment_decoder DUT (
+        .bcd(bcd),
+        .seven_segment(seven_segment)
+);
+
+initial
+begin
+        bcd = 4'd0;
+        #20 bcd = 4'd1;
+        #20 bcd = 4'd2;
+        #20 bcd = 4'd3;
+        #20 bcd = 4'd4;
+        #20 bcd = 4'd5;
+        #20 bcd = 4'd6;
+        #20 bcd = 4'd7;
+        #20 bcd = 4'd8;
+        #20 bcd = 4'd9;
+        #20 bcd = 4'd10;
+        #20 $stop;
+end
+
+endmodule
